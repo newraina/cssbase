7aff61dd398a6cb0eafd6f6786dbf2935abe8291
haoxin <coderhaoxin@outlook.com>
2016-07-08 00:09:49 +0800

    add rem.js

---
+++ rem.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+
+const start = 320
+const end = 420
+
+const bases = [320]
+const range = 10
+
+bases.forEach(base => {
+  const file = fs.createWriteStream(path.resolve(`rem-${base}.css`))
+  for (let i = start; i < end; i += range) {
+    const d = 16 / base
+    const v = `
+@media screen and (min-width: ${i}px) and (max-width: ${i + range - 1}px) {
+  html { font-size: ${d * i}px; }
+}
+`
+    file.write(v)
+  }
+})
